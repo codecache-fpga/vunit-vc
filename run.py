8166e61a47ee4c3a24b805266cdcabc3dfdf2c4f
Sebastian Hellgren <sebastian.abrahamsson@gmail.com>
2024-06-23 11:49:55 +0200

    Update run.py

--- run.py
+++ run.py
@@ -3,20 +3,12 @@
 # --------------------------------------------------------------------------------------------------
 
 import sys
-from pathlib import Path
 
-# Do PYTHONPATH insert() instead of append() to prefer any local repo checkout over any pip install
-REPO_ROOT = Path(__file__).parent.resolve()
-TSFPGA_PATH = REPO_ROOT / "tsfpga"
-VUNIT_PATH = REPO_ROOT / "vunit"
+from common import MODULES_PATH, SIM_OUTPUT_PATH, TSFPGA_PATH, VUNIT_PATH
+
 sys.path.insert(0, str(TSFPGA_PATH))
 sys.path.insert(0, str(VUNIT_PATH))
 
-OUTPUT_PATH = REPO_ROOT / "out"
-
-MODULES_PATHS = REPO_ROOT / "modules"
-
-# First party libraries
 from tsfpga.examples.simulation_utils import (
     SimulationProject,
     get_arguments_cli,
@@ -29,10 +21,10 @@ def main():
     you probably want to copy and modify this function. The other functions and classes
     should be reusable in most cases.
     """
-    cli = get_arguments_cli(default_output_path=OUTPUT_PATH)
+    cli = get_arguments_cli(default_output_path=SIM_OUTPUT_PATH)
     args = cli.parse_args()
 
-    modules = get_modules(MODULES_PATHS)
+    modules = get_modules(MODULES_PATH)
 
     simulation_project = SimulationProject(args=args)
     simulation_project.add_modules(args=args, modules=modules)
