60b5b53dfc74bfda3e560ea3400ae5320ae02b10
Sebastian Hellgren <sebastian.hellgren@gridtech.se>
2024-10-28 21:18:18 +0100

    Remove build.py and hdl modules

--- run.py
+++ run.py
@@ -5,7 +5,6 @@
 import sys
 
 from common import (
-    HDL_MODULES_MODULES_PATH,
     MODULES_PATH,
     REPO_ROOT,
     SIM_OUTPUT_PATH,
@@ -33,14 +32,10 @@ def main():
     # Run this every time to run as often as possible to keep vhdl_ls.toml updated
     create_vhdl_ls_configuration(REPO_ROOT, REPO_ROOT / "tmp", modules=modules)
 
-    names_avoid = set(["hard_fifo"]) if args.vivado_skip else set()
-
-    modules_no_sim = get_modules(HDL_MODULES_MODULES_PATH, names_avoid=names_avoid)
-
     simulation_project = SimulationProject(args=args)
-    simulation_project.add_modules(args=args, modules=modules, modules_no_sim=modules_no_sim)
+    simulation_project.add_modules(args=args, modules=modules)
 
-    if not args.vivado_skip:
+    if not args.vivado_skip and simulation_project.vunit_proj.get_simulator_name() != "nvc":
         simulation_project.add_vivado_simlib()
 
     simulation_project.vunit_proj.main()
